22c0c52a9b5b2e1e8ca06b3fea5559ef2dc06309
lelong199896 <144192988+lelong199896@users.noreply.github.com>
2024-01-04 09:26:13 +0700

    Add files via upload

---
+++ Spring Controller/spring-timezone-maven/src/main/java/com/example/springtimezonemaven/controller/TimeController.java
@@ -0,0 +1,24 @@
+package com.example.springtimezonemaven.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.Date;
+import java.util.TimeZone;
+
+@Controller
+public class TimeController {
+    @GetMapping("/world-clock")
+    public String getTimeByTimezone(ModelMap modelMap, @RequestParam(name = "city", required = false, defaultValue = "Asia/Ho_Chi_minh") String city) {
+        Date date = new Date();
+        TimeZone local = TimeZone.getDefault();
+        TimeZone locale = TimeZone.getTimeZone(city);
+        long localTime = date.getTime() + (locale.getRawOffset() - local.getRawOffset());
+        date.setTime(localTime);
+        modelMap.addAttribute("city", city);
+        modelMap.addAttribute("date", date);
+        return "index";
+    }
+}
